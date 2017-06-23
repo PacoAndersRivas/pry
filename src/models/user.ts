0f14f83141e7bd711bffa53b23155bcb338c184d
pullyvan <pullyvan.kris@gmail.com>
2017-06-23 12:37:41 +0200

    add phone support

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,5 +5,6 @@ export interface User {
 
   email: string;
   password: string;
+  phone_number : string;
 
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import {User} from "../../models/user";
 import {AngularFireAuth} from "angularfire2/auth";
 import {async} from "rxjs/scheduler/async";
+import firebase from 'firebase';
+
 
 /**
  * Generated class for the LoginPage page.
@@ -19,31 +21,94 @@ export class LoginPage {
 
   user = {} as User;
 
-  constructor(private afAuth:AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
+  //captcha
+  public recaptchaVerifier:firebase.auth.RecaptchaVerifier;
+
+
+  constructor(private afAuth:AngularFireAuth, public navCtrl: NavController, public navParams: NavParams, public alertCtrl:AlertController) {
 
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
   }
 
   async login(user : User){
 
-
     try {
-      const result = await this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
-      if(result) {
-        this.navCtrl.push('HomePage');
+      if (!user.email && !user.password) {
+
+        const appVerifier = this.recaptchaVerifier;
+        const phoneNumberString = "+" + user.phone_number;
+
+
+        firebase.auth().signInWithPhoneNumber(phoneNumberString, appVerifier)
+          .then(confirmationResult => {
+            // SMS sent. Prompt user to type the code from the message, then sign the
+            // user in with confirmationResult.confirm(code).
+            let prompt = this.alertCtrl.create({
+              title: 'Enter the Confirmation code',
+              inputs: [{name: 'confirmationCode', placeholder: 'Confirmation Code'}],
+              buttons: [
+                {
+                  text: 'Cancel',
+                  handler: data => {
+                    console.log('Cancel clicked');
+                  }
+                },
+                {
+                  text: 'Send',
+                  handler: data => {
+                    confirmationResult.confirm(data.confirmationCode)
+                      .then(function (result) {
+                        // User signed in successfully.
+                        console.log(result.user);
+                        console.log('SMS Sent');
+
+                        this.navCtrl.setRoot('HomePage');
+
+
+                        // ...
+                      }).catch(function (error) {
+                      // User couldn't sign in (bad verification code?)
+                      // ...
+                    });
+                  }
+                }
+              ]
+            });
+            prompt.present();
+          })
+          .catch(function (error) {
+            console.error("SMS not sent", error);
+          });
+
+
+      } else {
+
+        const result = await this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+
+        if (result) {
+          this.navCtrl.setRoot('HomePage');
+        }
       }
+
     }
-    catch(e)
+    catch
+      (e)
     {
       console.log(e)
     }
+
   }
+
+
+
   register(){
     this.navCtrl.push('RegisterPage');
-
-
   }
+
+
+
+
 }
